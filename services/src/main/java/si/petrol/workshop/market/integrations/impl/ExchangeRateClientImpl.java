008de62627c2d9bef7c006efc8e1c235e7285fff
Bobo <borut@komac.si>
2017-10-25 13:51:50 +0200

    rest client...

--- services/src/main/java/si/petrol/workshop/market/integrations/impl/ExchangeRateClientImpl.java
+++ services/src/main/java/si/petrol/workshop/market/integrations/impl/ExchangeRateClientImpl.java
@@ -3,16 +3,30 @@ package si.petrol.workshop.market.integrations.impl;
 import si.petrol.workshop.market.integrations.ExchangeRatesClient;
 import si.petrol.workshop.market.integrations.beans.Rate;
 
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
 
-public class ExchangeRateClientImpl implements ExchangeRatesClient{
+@ApplicationScoped
+public class ExchangeRateClientImpl implements ExchangeRatesClient {
 
+
+    private Client client;
+
+    @PostConstruct
+    private void init() {
+        client = ClientBuilder.newClient();
+    }
 
     @Override
     public Rate findRateByCurrency(String currency) {
 
+        return client.target("http://api.fixer.io/latest")
+                .queryParam("base", currency)
+                .queryParam("symbols", "EUR")
+                .request(MediaType.APPLICATION_JSON_TYPE).get(Rate.class);
 
-
-        return null;
     }
 }
